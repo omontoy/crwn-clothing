1fde3aba8dfe9b23c45fe6e88cac3bd8a768c1be
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2021-11-08 22:38:23 -0500

    Change the CustomButton component type to button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -68,6 +68,7 @@ class SignIn extends Component {
             </CustomButton>
 
             <CustomButton 
+              type="button"
               onClick={signInWithGoogle} 
               isGoogleSignIn
             >
